fbb8bc5d4406474fe9c980710ccea188e60caba7
Prateek Surana <prateeksurana3255@gmail.com>
2024-06-23 17:06:01 +0530

    :tada: Init shadcn/ui and tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div className='h-full w-full flex items-center justify-center'>
+      <h1 className='text-4xl font-semibold'>Building Unreal Books 🏗️</h1>
+    </div>
   </React.StrictMode>,
 )
