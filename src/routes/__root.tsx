e185c3f67a89055bd52f066f434a27187cd7cc40
Prateek Surana <prateeksurana3255@gmail.com>
2024-06-25 10:01:33 +0530

    :sparkles: Add root route

---
+++ src/routes/__root.tsx
@@ -0,0 +1,37 @@
+import { createRootRoute } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <div className="flex h-full">
+      <nav className="w-64 bg-white p-6">
+        <div className="flex items-center space-x-2">
+          <BookIcon className="h-6 w-6" />
+          <span className="font-semibold">Unreal Books</span>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+function BookIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20" />
+    </svg>
+  );
+}
